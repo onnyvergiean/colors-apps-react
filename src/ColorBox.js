bb4572f0ae35aa89c37549296cbd9e72cce97312
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2022-02-13 12:58:45 +0700

    Add Style ColorBox and Add CopyToClipboard

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./ColorBox.css";
 export default class ColorBox extends Component {
   render() {
+    const { background, id } = this.props;
     return (
-      <div
-        className="ColorBox"
-        style={{ background: this.props.background }}
-        key={this.props.id}
-      >
-        <span>{this.props.id}</span>
-        <span>MORE</span>
-      </div>
+      <CopyToClipboard text={background}>
+        <div className="ColorBox" style={{ background }} key={id}>
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{id}</span>
+            </div>
+            <button className="copy-button">Copy</button>
+          </div>
+          <span className="see-more">More</span>
+        </div>
+      </CopyToClipboard>
     );
   }
 }
