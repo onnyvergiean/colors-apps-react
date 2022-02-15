7dbb16d70aac4e4d818c5f4567b02fe7eaf8f54a
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2022-02-15 09:55:19 +0700

    Add Some Styling for MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import MiniPalette from "./MiniPalette";
+
 import seedColor from "./seedColor";
 
 export default function PaletteList() {
-  let palettes = seedColor.map((palette) => (
-    <p>
-      <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-    </p>
-  ));
+  let palettes = seedColor.map((palette) => <MiniPalette {...palette} />);
 
   return <div>{palettes}</div>;
 }
