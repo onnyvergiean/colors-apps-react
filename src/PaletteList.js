b67de997a1b518d06ac08637df2b16973d3a0ebf
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2022-02-15 19:31:14 +0700

    Start Styiling PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,12 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
+import { withStyles } from "@material-ui/core/styles";
 
 import MiniPalette from "./MiniPalette";
-
 import seedColor from "./seedColor";
+const styles = {
+  root: {
+    backgroundColor: "blue",
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  nav: { display: "flex", width: "100%", justifyContent: "space-between" },
+  palette: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3,30%)",
+    gridGap: "5%",
+  },
+};
 
-export default function PaletteList() {
+function PaletteList(props) {
+  const { classes } = props;
   let palettes = seedColor.map((palette) => <MiniPalette {...palette} />);
 
-  return <div>{palettes}</div>;
+  return (
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Colors</h1>
+        </nav>
+        <div className={classes.palette}>{palettes}</div>
+      </div>
+    </div>
+  );
 }
+
+export default withStyles(styles)(PaletteList);
